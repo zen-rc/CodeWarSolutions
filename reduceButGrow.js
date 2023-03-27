22eaaf900443f31044c5172fb71dcc64f2b2e62d
zen-rc <zenmalik.rc@gmail.com>
2023-03-26 21:26:57 -0400

    committing with missing code

---
+++ reduceButGrow.js
@@ -0,0 +1,36 @@
+//INSTRUCTIONS
+//Given a non-empty array of integers, return the result of multiplying the values together in order.
+
+
+//PSUEDO CODE EXPLANATION
+
+
+//MY SOLUTION
+const grow = (arr) => {
+    const redarr = arr.reduce((a,b) => a * b)
+    
+    return redarr
+}
+
+
+//1
+
+//2
+
+//3
+
+//4
+
+//5
+
+//6
+
+//7
+
+//8
+
+//9
+
+//10
+
+//OTHER SOLUTIONS
