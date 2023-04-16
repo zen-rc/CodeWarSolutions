1acb9a3050f1898b16fcdcf45e4af05b97dc7533
zen-rc <zenmalik.rc@gmail.com>
2023-04-16 10:30:00 -0400

    daily commit

---
+++ convertNumberToReversedArrayOfDigits.js
@@ -0,0 +1,34 @@
+//INSTRUCTIONS
+//Given a random non-negative number, you have to return the digits of 
+//this number within an array in reverse order.
+
+
+//PSUEDO CODE EXPLANATION
+// turn number into string so that you may split it into an array
+// reverse the array
+// use map to convert the array values into numbers
+
+//MY SOLUTION
+const digitize = n => n.toString().split('').reverse().map(Number)
+
+//1
+
+//2
+
+//3
+
+//4
+
+//5
+
+//6
+
+//7
+
+//8
+
+//9
+
+//10
+
+//OTHER SOLUTIONS
---
+++ thirdAngleTriangle.js
@@ -0,0 +1,39 @@
+// //INSTRUCTIONS
+// //You are given two interior angles (in degrees) of a triangle.
+// Write a function to return the 3rd.
+// Note: only positive integers will be tested.
+
+
+//PSUEDO CODE EXPLANATION
+
+
+//MY SOLUTION
+function otherAngle(a, b) {
+     const c = 180 - (a + b)
+     return c
+  }
+
+
+//1
+
+//2
+
+//3
+
+//4
+
+//5
+
+//6
+
+//7
+
+//8
+
+//9
+
+//10
+
+//OTHER SOLUTIONS
+
+const otherAngle = (a, b) => 180 - (a + b);
