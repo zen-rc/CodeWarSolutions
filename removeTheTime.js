3a2ddb66c3391e08988b29dd8994649f5efaa844
zen-rc <zenmalik.rc@gmail.com>
2023-04-21 10:30:00 -0400

    daily commit

---
+++ removeTheTime.js
@@ -0,0 +1,41 @@
+//INSTRUCTIONS
+// Task Description
+// You're re-designing a blog, and the blog's posts have the Weekday Month Day, time format for 
+// showing the date and time when a post was made, e.g., Friday May 2, 7pm.
+
+// You're running out of screen real estate, and on some pages you want to display a shorter format,
+// Weekday Month Day that omits the time.
+
+// Write a function that takes the website date/time in its original string format and returns
+// the shortened format.
+
+//PSUEDO CODE EXPLANATION
+
+
+//MY SOLUTION
+function shortenToDate(longDate) {
+    const date = longDate.split(",")[0]
+    return date
+  }
+  
+//1
+
+//2
+
+//3
+
+//4
+
+//5
+
+//6
+
+//7
+
+//8
+
+//9
+
+//10
+
+//OTHER SOLUTIONS
