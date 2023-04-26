5a639f76aef0898f6c879cc805b9ad34ca825c06
zen-rc <zenmalik.rc@gmail.com>
2023-04-26 10:30:00 -0400

    daily commit

---
+++ isHeGonnaSurvive.js
@@ -0,0 +1,43 @@
+//INSTRUCTIONS
+//A hero is on his way to the castle to complete his mission. 
+// However, he's been told that the castle is surrounded with a couple of powerful dragons!
+// each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry..
+// Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific
+// given number of dragons, will he survive?
+// Return true if yes, false otherwise :)
+
+
+//PSUEDO CODE EXPLANATION
+
+
+//MY SOLUTION
+function hero(bullets, dragons){
+    if(dragons * 2 <= bullets) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+
+//1
+
+//2
+
+//3
+
+//4
+
+//5
+
+//6
+
+//7
+
+//8
+
+//9
+
+//10
+
+//OTHER SOLUTIONS
