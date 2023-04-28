7d6fde9fd4f4707018a3dc50570a15aff69eabdf
zen-rc <zenmalik.rc@gmail.com>
2023-04-28 11:22:50 -0400

    daily commit

---
+++ convertNumberToString.js
@@ -0,0 +1,34 @@
+//INSTRUCTIONS
+//We need a function that can transform a number (integer) into a string.
+// What ways of achieving this do you know?
+
+//PSUEDO CODE EXPLANATION
+
+
+//MY SOLUTION
+function numberToString(num) {
+    const str = num.toString()
+    return str
+   }
+   
+//1
+
+//2
+
+//3
+
+//4
+
+//5
+
+//6
+
+//7
+
+//8
+
+//9
+
+//10
+
+//OTHER SOLUTIONS
