f2ccb558d148ed035091f2dcca0e72cb7029212f
zen-rc <zenmalik.rc@gmail.com>
2023-04-29 19:41:36 -0400

    daily commit

---
+++ reversedString.js
@@ -0,0 +1,31 @@
+//INSTRUCTIONS
+//Complete the solution so that it reverses the string passed into it.
+
+
+//PSUEDO CODE EXPLANATION
+
+
+//MY SOLUTION
+const solution = str => str.split("").reverse().join("")
+
+//1
+
+//2
+
+//3
+
+//4
+
+//5
+
+//6
+
+//7
+
+//8
+
+//9
+
+//10
+
+//OTHER SOLUTIONS
