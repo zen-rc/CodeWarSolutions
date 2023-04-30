3cb92d87766c674324c76094b5f9a1565403936b
zen-rc <zenmalik.rc@gmail.com>
2023-04-30 12:54:15 -0400

    daily commit

---
+++ boolValueToStrings.js
@@ -0,0 +1,32 @@
+//INSTRUCTIONS
+//Complete the method that takes a boolean value and return a "Yes" string for true,
+// or a "No" string for false.
+
+
+//PSUEDO CODE EXPLANATION
+
+
+//MY SOLUTION
+const boolToWord = bool => bool ? "Yes" : "No"
+
+//1
+
+//2
+
+//3
+
+//4
+
+//5
+
+//6
+
+//7
+
+//8
+
+//9
+
+//10
+
+//OTHER SOLUTIONS
