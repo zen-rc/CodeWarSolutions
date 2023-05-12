ce6b14a5961ec0a7cf24c057102299c4b7638edb
Zen Malik <125770666+zen-rc@users.noreply.github.com>
2023-05-12 17:12:17 -0400

    Update convertNumberToReversedArrayOfDigits.js

--- convertNumberToReversedArrayOfDigits.js
+++ convertNumberToReversedArrayOfDigits.js
@@ -12,23 +12,23 @@
 const digitize = n => n.toString().split('').reverse().map(Number)
 
 //1
-
+const digitize = n => n.toString().split('').reverse().map(Number)
 //2
-
+const digitize = n => n.toString().split('').reverse().map(Number)
 //3
-
+const digitize = n => n.toString().split('').reverse().map(Number)
 //4
-
+const digitize = n => n.toString().split('').reverse().map(Number)
 //5
-
+const digitize = n => n.toString().split('').reverser().map(Number)
 //6
-
+const digitize = n => n.toString().split('').reverse().map(Number)
 //7
-
+const digitize = n => n.toString().split('').reverser().map(Number)
 //8
-
+const digitize = n => n.toString().split('').reverse().map(Number)
 //9
-
+const digitize = n => n.toString().split('').reverse().map(Number)
 //10
-
+const digitize = n => n.toString().split('').reverse().map(Number)
 //OTHER SOLUTIONS
