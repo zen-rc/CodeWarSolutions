4658044423521fc46b56c2e733559847a491bd04
Zen Malik <125770666+zen-rc@users.noreply.github.com>
2023-05-12 17:45:36 -0400

    Update evenOrOdd.js

--- evenOrOdd.js
+++ evenOrOdd.js
@@ -7,26 +7,26 @@
 
 
 //MY SOLUTION
-const evenOrOdd = (number) => number % 2 === 0 ? 'Even' : 'Odd'
+const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd'
 
 //1
-
+const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd'
 //2
-
+const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd'
 //3
-
+const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd'
 //4
-
+const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd'
 //5
-
+const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd'
 //6
-
+const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd'
 //7
-
+const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd'
 //8
-
+const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd'
 //9
-
+const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd'
 //10
-
+const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd'
 //OTHER SOLUTIONS
