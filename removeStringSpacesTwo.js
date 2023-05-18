c6e233ea9d093fc6a23ea645b04ea944ddcd0aa3
zen-rc <zenmalik.rc@gmail.com>
2023-05-17 20:32:06 -0400

    daily commit

---
+++ removeStringSpacesTwo.js
@@ -0,0 +1,12 @@
+// instructions
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// psuedo code
+
+// solutions
+const stringToNumber = str => Number(str)
+
+// other solutions
+var stringToNumber = function(str){
+  return parseInt(str);
+}
