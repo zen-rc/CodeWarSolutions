2eb149f4875ebb8aca61bdef4e8750d3e66ce062
zen-rc <zenmalik.rc@gmail.com>
2023-05-18 20:34:29 -0400

    daily commit

---
+++ reversalsPractice.js
@@ -0,0 +1,604 @@
+// CHALLENGE 1 //
+    //Reverse a String
+
+// techique one
+function reverseString(str) {
+    str.split('').reverser().join('')
+}
+
+// technique two
+function reverseString(str) {
+    let revString = ''
+    for(let i = str.length; i >=0; i--) {
+        revString = revString + str[i]
+    }
+    return revString
+}
+// technique three
+function reverseString(str) {
+    let revString = ''
+    for(let i = 0; i <= str.length - 1; i++) {
+        revString = str[i] + revString
+    }
+    return revString
+}
+
+// technique four
+function reverseString(str) {
+    let revString = ''
+    for(let char of str) {
+        revString = char + revString
+    }
+    return revString
+}
+
+// technique five 
+function reverseString(str) {
+    let revString = ''
+
+    str.split('').forEach((char) => {
+        revString = char + revString
+    })
+    return revString
+}
+
+
+
+// technique two practice ----------------------------
+function reverseString(str) {
+    let revString = ''
+    for(let i = str.length - 1; i >=0; i--) {
+        revString = revString + str[i]
+    }
+    return revString
+}
+
+// 1
+function reverseString(str) {
+    let revString = ''
+    for(let i = str.length - 1; i >= 0; i--) {
+        revString = revString + str[i]
+    }
+    return revString
+}
+// 2
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = str.length - 1; i >= 0; i--) {
+        revString = revString + str[i]
+    }
+    return revString
+}
+// 3
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = str.length - 1; i >=0; i--) {
+        revString = revString + str[i]
+    }
+    return revString
+}
+// 4
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = str.length - 1; i >= 0; i--) {
+        revString = revString + str[i]
+    }
+    return revString
+}
+// 5
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = str.length - 1; i >= 0; i--) {
+        revString = revString + str[i]
+    }
+    return revString
+}
+// 6
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = str.length - 1; i >= 0; i--) {
+        revString = revString + str[i]
+    }
+    return revString
+}
+// 7
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = str.length - 1; i >= 0; i--) {
+        revString = revString + str[i]
+    }
+    return revString
+}
+// 8
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = str.length - 1; i >= 0; i-- ) {
+        revString = revString + str[i]
+    }
+    return revString 
+}
+// 9
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = str.lenght - 1; i >= 0; i--) {
+        revString = revString + str[i]
+    }
+    return revString
+}
+// 10
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = str.length - 1; i >= 0; i--) {
+        revString = revString + str[i]
+    }
+}
+
+
+// technique three practice ----------------------------
+function reverseString(str) {
+    let revString = ''
+    for(let i = 0; i <= str.length - 1; i++) {
+        revString = str[i] + revString
+    }
+    return revString
+}
+
+// 1
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = 0; i <= str.length -1; i++) {
+        revString = str[i] + revString
+    }
+    return revString
+}
+// 2
+function reverseString(str){
+    let revString = ''
+
+    for(let i = 0; i <= str.length -1; i++) {
+        revString = str[i] + revString
+    }
+    return revString
+}
+// 3
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = 0; i <= str.length -1; i++) {
+        revString = str[i] + revString
+    }
+    return revString
+}
+// 4
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = 0; i <= str.length -1; i++) {
+        revString = str[i] + revString
+    }
+    return revString
+}
+// 5
+function reverseString(str) {
+    let revString = ''   
+    
+    for(let i = 0; i <= str.length -1; i++) {
+        revString = str[i] + revString
+    }
+    return revString
+}
+// 6
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = 0; i <= str.length; i++) {
+        revString = str[i] + revString
+    }
+    return revString
+}
+// 7
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = 0; i <= str.length; i++) {
+        revString = str[i] + revString
+    }
+    return revString
+}
+// 8
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = 0; i <= str.length - 1; i++) {
+        revString = str[i] + revString
+    }
+    return revString
+}
+// 9
+function reverseString(str) {
+    let revString = ''
+
+    for(let i = 0; i <= str.length -1; i++) {
+        revString = str[i] + revString
+    }
+    return revString
+}
+// 10
+function reverseSTring(str) {
+    let revString = ''
+
+    for(let i = 0; i <= str.length -1; i++) {
+        revString = str[i] + revString
+    }
+    return revString
+}
+// technique four practice ----------------------------
+function reverseString(str) {
+    let revString = ''
+    for(let char of str) {
+        revString = char + revString
+    }
+    return revString
+} 
+
+// 1
+function reverseString(str) {
+    let revString = ''
+
+    for(let char of str) {
+        revString = char + revString
+    }
+    return revString
+}
+// 2
+function reverseString(str) {
+    let revString = ''
+
+    for(let char of str) {
+        revString = char + revString
+    }
+    return revString
+}
+// 3
+function reverseString(str) {
+    let revString = ''
+
+    for(let char of str) {
+        revString = char + revString
+    }
+    return revString
+}
+// 4
+function reverseString(str) {
+    let revString = ''
+
+    for(let char of str){
+        revString = char + revString
+    }
+    return revString
+}
+// 5
+function reverseString(str) {
+    let revString = ''
+
+    for(let char of str) {
+        revString = char + revString
+    } return revString
+}
+
+// 6
+function reverseString(str){
+    let revString = ''
+
+    for(let char of str) {
+        revString = char + revString
+    }
+    return revString
+}
+// 7
+function reverseString(str) {
+    let revString = ''
+
+    for(let char of str) {
+        revString = char + revString
+    }
+    return revString
+}
+// 8
+function reverseString(str) {
+    let revString = ''
+
+    for(let char of str) {
+        revString = char + revString
+    }
+    return revString
+}
+// 9
+function reverseString(str) {
+    let revString = ''
+
+    for(let char of str) {
+        revString = char + revString
+    }
+    return revString
+}
+// 10
+function reverseString(str) {
+    let revString = ''
+
+    for(let char of str) {
+        revString = char + revString
+    }
+    return revString
+}
+
+// technique five practice ----------------------------
+function reverseString(str) {
+    let revString = ''
+
+    str.split('').forEach((char) => {
+        revString = char + revString
+    })
+    return revString
+}
+
+// 1
+function reverseString(str) {
+    let revString = ''
+
+    str.split('').forEach((char) => {
+        revString = char + revString
+    })
+    return revString
+}
+// 2
+function reverseString(str) {
+    let revString = ''
+
+    str.split('').forEach((char) => {
+        revString = char + revString
+    })
+    return revString
+}
+// 3
+function reverseString(str) {
+    let revString = ''
+
+    str.split('').forEach((char) => {
+        revString = char + revString
+    })
+    return revString
+}
+// 4
+function reverseString(str) {
+    let revString = ''
+
+    str.split('').forEach((char) => {
+        revString = char + revString
+    })
+    return revString
+}
+// 5
+function reverseString(str) {
+    let revString = ''
+
+    str.split('').forEach((char) => {
+        revString = char + revString
+    })
+    return revString
+}
+// 6
+function reverseString(str) {
+    let revString = ''
+
+    str.split('').forEach((char) => {
+        revString = char + revString
+    })
+    return revString
+}
+// 7
+function reverseString(str) {
+    let revString = ''
+
+    str.split('').forEach((char) => {
+        revString = char + revString
+    })
+    return revString
+}
+// 8
+function reverseString(str) {
+    let revString = ''
+
+    str.split('').forEach((char) => {
+        revString = char + revString
+    })
+    return revString
+}
+// 9
+function reverseString(str) {
+    let revString = ''
+
+    str.split('').forEach((char) => {
+        revString = char + revString
+    })
+    return revString
+}
+// 10
+function reverseString(str) {
+    let revString = ''
+
+    str.split('').forEach((char) => {
+    })
+    return revString
+}
+
+
+
+
+
+// CHALLENGE 2 //
+    //Validate a Palindrome
+    // return true if palindrome, false if not.
+
+// techique one
+function isPalindrome(str) {
+    const revString = str.split('').reverse().join('')
+
+    return revString === str
+}
+
+// techique one practice ----------------------------
+function isPalindrome(str) {
+    const revString = str.split('').reverse().join('')
+
+    return revString === str
+}
+
+// 1
+function isPalindrome(str) {
+    const revString = str.split('').reverse().join('')
+
+    return revString === str
+}
+// 2
+function isPalindrome(str) {
+    const revString = str.split('').reverse().join('')
+
+    return revString === str
+}
+// 3
+function isPalindrome(str) {
+    const revString = str.split('').reverse().join('')
+
+    return revString === str
+}
+// 4
+function isPalindrome(str) {
+    const revString = str.split('').reverse().join('')
+
+    return revString === str
+}
+// 5
+function isPalindrome(str) {
+    const revString = str.split('').reverse().join('')
+
+    return revString === str
+}
+// 6
+function isPalindrome(str) {
+    const revString = str.split('').reverse().join('')
+
+    return revString === str
+}
+// 7
+function isPalindrome(str) {
+    const revString = str.split('').reverse().join('')
+
+    return revString === str
+}
+// 8
+function isPalindrome(str) {
+    const revString = str.split('').reverse().join('')
+
+    return revString === str
+}
+// 9
+function isPalindrome(str) {
+    const revString = str.split('').reverse().join()
+
+    return revString === str
+}
+// 10
+function isPalindrome(str) {
+    const revString = str.split('').reverse().join('')
+
+    return revString === str
+}
+// CHALLENGE 2 //
+    //return an integer in reverse
+
+// techique one
+function reverseInt(int) {
+    const revString = int.toString().split('').reverse().join('')
+
+    return parseInt(revString)
+}
+
+// techique one practice --------------------------
+
+// 1
+function reverseInt(int) {
+    const revString = int.toString().split('').reverse().join('')
+
+    return parseInt(revString)
+}
+// 2
+function reverseInt(int) {
+    const revString = int.toString().split('').reverse().join('')
+
+    return parseInt(revString)
+}
+// 3
+function reverseInt(int) {
+    const revString = int.toString().split('').reverse().join('')
+
+    return parseInt(revString)
+}
+// 4
+function reverseInt(int) {
+    const revString = int.toString().spl
+}
+// 5
+
+// 6
+
+// 7
+
+// 8
+
+// 9
+
+// 10
+
+// PREP PRACTICE
+
+// 1
+
+// 2
+
+// 3
+
+// 4
+
+// 5
+
+// 6
+
+// 7
+
+// 8
+
+// 9
+
+// 10
+
+// create a function
+// return most common character in the array
+
+// string will ! be empty
+// ajax 
+// return string "a"
+
+function commonChar(str) {
+
+}
