a55079f3c0c7461c8d5604f93aa26f49592e1740
zen-rc <zenmalik.rc@gmail.com>
2023-06-04 12:57:28 -0400

    daily commit

---
+++ convertBooleanToString.js
@@ -0,0 +1,7 @@
+// Instructions
+    // Implement a function which convert the given boolean value 
+    // into its string representation.
+
+// My solution
+const booleanToString = b => b.toString()
+
