2b3d2bbd42366b05f89b8d49b32123b5136456ff
zen-rc <zenmalik.rc@gmail.com>
2023-06-21 11:11:58 -0400

    daily commit

---
+++ removeExclamationMarks.js
@@ -0,0 +1,7 @@
+// instructions
+    // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// my solution
+const removeExclamationMarks = s => s.replace('/!/g', '')
+
+// practice
