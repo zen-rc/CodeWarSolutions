6ca0463e6e46171826f32c2a2d531cc30ba610e6
zen-rc <zenmalik.rc@gmail.com>
2023-06-21 11:21:59 -0400

    practice

--- removeExclamationMarks.js
+++ removeExclamationMarks.js
@@ -2,6 +2,36 @@
     // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
 // my solution
-const removeExclamationMarks = s => s.replace('/!/g', '')
+const removeExclaim = s => s.replace('/!/g', '')
 
 // practice
+
+    // 1
+const removeExclaim = s => s.replace('/!/g', '' )
+
+    // 2
+const removeExclaim = s => s.replace('/!/g', '')
+
+    // 3
+const removeExclaim = s => s.replace('/!/g', '')
+
+    // 4
+const removeExclaim = s => s.replace('/!/g', '')
+
+    // 5
+const removeExclaim = s => s.replace('/!/g', '')
+
+    // 6
+const removeExclaim = s => s.replace('/!/g', '')
+
+    // 7
+const removeExclaim = s => s.replace('/!/g', '')
+
+    // 8
+const removeExclaim = s => s.replace('/!/g', '')
+
+    // 9
+const removeExclaim = s => s.replace('/!/g', '')
+
+    // 10
+const removeExclaim = s => s.replace('/!/g', '')
