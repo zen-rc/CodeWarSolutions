3024bf09d117255eb05c54abc20557e7244524f1
zen-rc <zenmalik.rc@gmail.com>
2024-03-25 12:00:00 -0400

    new challenge

---
+++ isThisATriangle.js
@@ -0,0 +1,37 @@
+// INSTRUCTIONS
+    // Implement a function that accepts 3 integer values a, b, c. 
+    // The function should return true if a triangle can be built with
+    // the sides of given length and false in any other case.
+
+// PREP
+    // passing three integers (sides of a triangle)
+    // returing a boolean
+    // example: input(1,2,2) --> output: true
+
+// PSUEDO CODE
+
+// MY SOLUTION
+const isTriangle = (a,b,c) => a + b > c && b + c > a && a + c > b 
+
+// 1
+
+// 2
+
+// 3
+
+// 4
+
+// 5
+
+// 6
+
+// 7
+
+// 8
+
+// 9
+
+// 10
+
+// OTHER SOLUTIONS
+
