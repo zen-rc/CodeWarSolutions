9ed5d3206ef7910e6b8c0f722a6f9caf3df4b07d
zen-rc <zenmalik.rc@gmail.com>
2024-04-15 12:00:00 -0400

    new challenge

---
+++ friendOrFoe.js
@@ -0,0 +1,39 @@
+// INSTRUCTIONS
+    // Make a program that filters a list of strings and returns a list 
+    // with only your friends name in it.
+    // If a name has exactly 4 letters in it, you can be sure that it has 
+    // to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// PREP
+    // passing an array
+    // returning an array of 4 character length
+    // Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+
+// PSUEDO CODE
+    // use filter method to check string length
+
+// MY SOLUTION
+const friend = friends => friends.filter((name) => name.length == 4)
+
+// 1
+
+// 2
+
+// 3
+
+// 4
+
+// 5
+
+// 6
+
+// 7
+
+// 8
+
+// 9
+
+// 10
